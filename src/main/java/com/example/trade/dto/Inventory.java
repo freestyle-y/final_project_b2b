a16ae51fdb8a9c701374e73675a7aefc06177eed
HwangGwihwan <hkh001107@gmail.com>
2025-08-20 16:15:08 +0900

    [귀환] feat: 재고 목록 조회, 재고 수량 수정

--- src/main/java/com/example/trade/dto/Inventory.java
+++ src/main/java/com/example/trade/dto/Inventory.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Inventory {
+	private int inventoryId;
 	private int productNo;
 	private int optionNo;
 	private int quantity;
